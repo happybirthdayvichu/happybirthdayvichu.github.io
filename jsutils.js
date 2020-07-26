076c196c052c0b3269d620c866de437a7d8b3e8f
Happy Birthday Vichu <68707373+happybirthdayvichu@users.noreply.github.com>
2020-07-26 20:41:56 +0530

    Add files via upload

--- jsutils.js
+++ jsutils.js
@@ -1,11 +1,18 @@
 function onloadevent() {
     let array1 = [["1","2","3","4","5","6","40","31", "9", "10", "45"],
-                  ["38","12","13","14","15","16","17","18", "19", "20", "42", "43"],
+                  ["38","13","14","15","16","17","18", "19", "20", "42", "43"],
                   ["21","7","22","23","24","25","26","27", "28", "29", "30"],
                   ["8","32","33","34","35","36","37","11", "39", "41", "44"]];
     for (row=0; row<4; row++) {
         for (i of array1[row]) {
-            document.getElementById("row"+(row+1)).innerHTML += "<img src='pics/"+i+".jpg' width='100%'>";
+            document.getElementById("row"+(row+1)).innerHTML += "<img src='srv/pics/"+i+".jpg' width='100%'>";
+        }
+    }
+}
+function onloadeventBuddies() {
+    for (row=0; row<4; row++) {
+        for (i=1;i<=11;i++) {
+            document.getElementById("brow"+(row+1)).innerHTML += "<img src='buddies/"+(i+(10*row))+".jpg' width='100%'>";
         }
     }
 }
